43754593d1357bd7d86dcd2849d2445d9b42c553
JITENDRA MALAKALAPALLI <jitendra8911@gmail.com>
2021-08-15 17:03:38 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router, Link, Switch, Route, useRouteMatch, useParams, withRouter
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to = "/">Home</Link>
+            </li>
+            <li>
+              <Link to = "/about">About</Link>
+            </li>
+            <li>
+              <Link to = "/users">Users</Link>
+            </li>
+            <li>
+              <Link to = "/moments">Moments</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/users">
+            <Users/>
+          </Route>
+          <Route  path={`/moments/:momentId`}>
+            <Moment/>
+          </Route>
+          <Route  path="/moments">
+            <Moments/>
+          </Route>
+          <Route  path="/">
+            <Home/>
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function About() {
+  return <h2>About</h2>
+}
+
+function Users() {
+  return <h2>Users</h2>
+}
+
+function Moments() {
+  let match = useRouteMatch();
+  console.log('Moments is called');
+
+  return (
+      <div>
+        <h2>Moments</h2>
+
+        <ul>
+          <li>
+            <Link to = {`/moments/chavithi-garelu-skewers`}>chavithi-garelu-skewers</Link>
+          </li>
+          <li>
+            <Link to = {`/moments/chicago-trip`}>chicago-trip</Link>
+          </li>
+        </ul>
+      </div>
+  )
+}
+
+function Moment() {
+  console.log('inside moment')
+  let { momentId } = useParams();
+  return (
+      <div>
+        <h3>Beautiful moment : {momentId}</h3>
+      </div>
+  )
 }
 
 export default App;
